cb97a47172144ba9ce2e616bc13b40b5ef0012fe
nullifiedcat <nullifiedcat@gmail.com>
2018-06-24 14:48:45 +0300

    create

--- cathook.sql
+++ cathook.sql
@@ -2,13 +2,13 @@
 
 -- DROP DATABASE cathook;
 
--- CREATE DATABASE cathook
---   WITH OWNER = postgres
---        ENCODING = 'UTF8'
---        TABLESPACE = pg_default
---        LC_COLLATE = 'en_US.UTF-8'
---        LC_CTYPE = 'en_US.UTF-8'
---        CONNECTION LIMIT = -1;
+ CREATE DATABASE cathook
+   WITH OWNER = nullifiedcat
+        ENCODING = 'UTF8'
+        TABLESPACE = pg_default
+        LC_COLLATE = 'en_US.UTF-8'
+        LC_CTYPE = 'en_US.UTF-8'
+        CONNECTION LIMIT = -1;
 
 CREATE TABLE "users" (
 	"id" serial NOT NULL,
